d4b4d809d65f2fc48f8aceec4dde9367148ff5d2
EngrishMajor <ken.yht@gmail.com>
2014-03-23 03:57:39 -0500

    i have absolutely no idea what i changed in the styling xml files(probably nothing)

--- gen/com/example/capacityprototype/R.java
+++ gen/com/example/capacityprototype/R.java
@@ -1337,11 +1337,7 @@ containing a value of this type.
         public static final int fragment_recent_search_page=0x7f03001d;
         public static final int fragment_search_location_page=0x7f03001e;
         public static final int fragment_search_page=0x7f03001f;
-        public static final int home_page=0x7f030020;
-        public static final int recent_search_page=0x7f030021;
-        public static final int search_location_page=0x7f030022;
-        public static final int search_page=0x7f030023;
-        public static final int support_simple_spinner_dropdown_item=0x7f030024;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int home_page=0x7f0c0000;
