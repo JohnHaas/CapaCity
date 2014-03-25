f92f2cfef398c56d8ac58a7bd331808911811fc5
E L <ekluo1@gmail.com>
2014-03-25 02:29:21 -0500

    basic search results layout

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,16 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 };
+
+exports.search = function(req, res){
+	res.render('search_results', {
+		title: 'CapaCity | Search Results',
+		query: req.query['q'] || "No query provided.",
+	});
+};
+
+exports.pageNotFound = function(req, res){
+	
+}
