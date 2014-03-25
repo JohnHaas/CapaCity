560b9166171bd181da1cba2ad89a73f54ad3b6c1
E L <ekluo1@gmail.com>
2014-03-25 03:40:09 -0500

    added mock data to search page

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,27 @@ exports.index = function(req, res){
 };
 
 exports.search = function(req, res){
+	// render with mock data
 	res.render('search_results', {
 		title: 'CapaCity | Search Results',
 		query: req.query['q'] || "No query provided.",
+		data: [
+			{
+				"name": "Sakanaya",
+				"wait": "5"
+			},
+			{
+				"name": "Benehana's",
+				"wait": "8"
+			},
+			{
+				"name": "Pizza Hut",
+				"wait": "35"
+			},
+		]
 	});
 };
 
 exports.pageNotFound = function(req, res){
-	
+
 }
