23165476440ecd916a34efc063a07b1dd44fa3c0
EngrishMajor <ken.yht@gmail.com>
2014-03-25 21:47:40 -0500

    added input placeholders

---
+++ routes/restaurant.js
@@ -0,0 +1,18 @@
+exports.newRestaurant = function(req, res) {
+	
+}
+
+/*
+Calculates the average wait time of a restaurant in minutes.
+@param p, average service time
+@param m, number of seats
+@param a, the interarrival time of customers in minutes
+*/
+function calculateWaitTime(p, m, a) {
+	var result = p / m;
+	var base = p / (m * a);
+	var exponent = Math.sqrt(2 * (m + 1)) - 1;
+	result *= Math.pow(base, exponent);
+	result /= 1 - (p / (m * a));
+	return result;
+}
